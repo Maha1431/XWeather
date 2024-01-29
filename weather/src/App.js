a530547edfa7f19d59319ac6c1360977d55260a2
Mahalakshmi <mmahaece07@gmail.com>
2024-01-29 14:28:36 +0530

    weather app created

---
+++ weather/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import Weather from "./weather";
+
+function App() {
+  return (
+    <div>
+    <Weather />
+    </div>
+  );
+}
+
+export default App;
---
+++ weather/src/weather.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./weather.css";
+
+function Weather() {
+  const [cityName, setCityName] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  
+
+
+  const apiKey = "edda11a73b81466b905151133232711";
+  const apiUrl = "https://api.weatherapi.com/v1/current.json";
+
+  const fetchData = async () => {
+    try {
+        setLoading(true);
+      const response = await axios.get(apiUrl, {
+        params: {
+          key: apiKey,
+          q: cityName,
+        },
+      });
+
+      setWeatherData(response.data);
+     
+    } catch (error) {
+      setWeatherData(null);
+      window.alert("Failed to fetch weather data");
+       
+    }
+    finally{
+        setLoading(false)
+    }
+  };
+
+
+
+  return (
+    <div className="container">
+      <h1>Weather App</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter city name"
+          className="input"
+          value={cityName}
+          onChange={(e) => setCityName(e.target.value)}
+        />
+        <button onClick={fetchData} className="button">
+          Search
+        </button>
+      </div>
+
+      {loading && <p>Loading data...</p>}
+
+       {weatherData && (
+        <div className="details">
+          <div className="weather-card">
+            <p>Temperature </p>
+            <p>{weatherData.current.temp_c}°C</p>
+          </div>
+          <div className="weather-card">
+            <p>Humidity</p>
+            <p>{weatherData.current.humidity}%</p>
+          </div>
+          <div className="weather-card">
+            <p>Condition</p>
+            <p>{weatherData.current.condition.text}</p>
+          </div>
+          <div className="weather-card">
+            <p>Wind Speed</p>
+            <p> {weatherData.current.wind_kph}km/h</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Weather;
